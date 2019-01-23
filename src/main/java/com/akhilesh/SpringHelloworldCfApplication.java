3c06c1a0e04a0733b621846c89e909af92caae86
akhilesh <akhilespande@gmail.com>
2019-01-23 15:13:46 +0530

    added logger and security

--- src/main/java/com/akhilesh/SpringHelloworldCfApplication.java
+++ src/main/java/com/akhilesh/SpringHelloworldCfApplication.java
@@ -1,5 +1,6 @@
 package com.akhilesh;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +16,14 @@ public class SpringHelloworldCfApplication {
 		SpringApplication.run(SpringHelloworldCfApplication.class, args);
 	}
 }
-
+@Slf4j
 @RestController
 class MessageRestController {
 
 	@RequestMapping("/hello")
 	String getMessage(@RequestParam(value = "name") String name) {
 		String rsp = "Hi " + name + " : responded on - " + new Date();
-		System.out.println(rsp);
+		log.info(rsp);
 		return rsp;
 	}
 }
